b386b35c112f516da609bedb470d30bfdc76d7a9
softguidetech <96677990+softguidetech@users.noreply.github.com>
2023-11-06 21:32:41 +0300

    Update __manifest__.py

--- school_figma/__manifest__.py
+++ school_figma/__manifest__.py
@@ -19,6 +19,7 @@
         ]
     },
     'version': '0.1',
+ "images": ['static/description/website.png'],
 
     'depends': ['base','portal','web','website', 'website_sale','website_blog'],
     'data': [
